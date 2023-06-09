e4214dcccc203b63484cae2c73843f16fd7831d3
Christian <christianryuji@hotmail.com>
2023-06-09 00:07:41 -0300

    feat: task 2 finished

---
+++ pets/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Sex_choice
+from groups.serializers import GroupSerializer
+from traits.serializers import TraitSerializer
+
+
+class PetSerializer(serializers.Serializer):
+    id: serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    age = serializers.IntegerField()
+    weight = serializers.FloatField()
+    sex = serializers.ChoiceField(choices=Sex_choice.choices)
+
+    group = GroupSerializer()
+    trait = TraitSerializer()
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Request, Response, status
+from .models import Pet, Sex_choice
+from django.forms.models import model_to_dict
 
-# Create your views here.
+
+class PetView(APIView):
+    def get(self, request: Request) -> Response:
+        pets = Pet.objects.all()
+
+        pet_list = []
+
+        for pet in pets:
+            pets_dict = model_to_dict(pet)
+            pet_list.append(pets_dict)
